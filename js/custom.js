e1d171066dc39e22c06f048009179ed3a5679c9e
Joshua Green <joshuagreen2312@gmail.com>
2016-02-09 14:56:58 +0000

    further refactoring of generic stuff

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,38 @@
 var konami = [38,38,40,40,37,39,37,39,66,65];
 var index = 0; 
 
+var Airhorn = function () {
+    this.x = Math.random() * $(document).width();
+    this.y = Math.random() * $(window).height();
+
+    this.airhorn_id = "airhorn" + this.airhorn_id;
+    Airhorn.numAirhorns++;
+
+    this.elem = this.genElement();
+
+};
+
+Airhorn.prototype.dance = function() {
+  console.log("Hello, I'm " + this.firstName);
+};
+
+Airhorn.prototype.genElement = function() {
+
+    var image = document.createElement("IMG");
+    image.alt = "dankness";
+    image.setAttribute('class', 'airhorn');
+    image.setAttribute('id', this.airhorn_id);
+    image.src="/res/airhorn.png";
+
+    $(image).css('top', this.y);
+    $(image).css('left', this.x);
+
+    return image;
+};
+
+Airhorn.numAirhorns = 0;
+
+
 $(document).keydown(function(e){
     if(e.which == konami[index]){
         index++;
@@ -28,12 +60,7 @@ function konami_entered(){
  function air_horns(){
     var audio = new Audio('/res/darude.mp3'); 
     audio.play();
-
-    var image = document.createElement("IMG");
-    image.alt = "dankness";
-    image.setAttribute('class', 'airhorn');
-    image.setAttribute('id', 'airhorn1');
-    image.src="/res/airhorn.png";
-    $("body").append(image);
-    $("#airhorn1").effect("shake");
+    
+    var airhorn = new Airhorn();
+    $("body").append(airhorn.elem);
  }
