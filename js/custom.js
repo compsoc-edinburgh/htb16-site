ab94c9eb8cec180448a95d4039dfee16b9908e9c
Joshua Green <joshuagreen2312@gmail.com>
2016-02-09 04:08:08 +0000

    yet further generic changes...

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,6 @@ var konami = [38,38,40,40,37,39,37,39,66,65];
 var index = 0; 
 
 $(document).keydown(function(e){
-
-
     if(e.which == konami[index]){
         index++;
     }else{
@@ -20,9 +18,22 @@ $(document).keydown(function(e){
 function konami_entered(){
     ga('send', 'event', 'konami', 'konami_entered');
     $("body").addClass('comic-sans');
+    air_horns();
     clippy.load('Clippy', function(agent) {
         agent.show();
         agent.moveTo($(document).width()-400, 50);
         agent.speak("Looks like you're signing up for a hackathon, need any help?");
     });
 }
+ function air_horns(){
+    var audio = new Audio('/res/darude.mp3'); 
+    audio.play();
+
+    var image = document.createElement("IMG");
+    image.alt = "dankness";
+    image.setAttribute('class', 'airhorn');
+    image.setAttribute('id', 'airhorn1');
+    image.src="/res/airhorn.png";
+    $("body").append(image);
+    $("#airhorn1").effect("shake");
+ }
